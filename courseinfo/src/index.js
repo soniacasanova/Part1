78b8cee1b4a3708ca7e887ee487db73622073513
“soniacasanova” <soniacasanovar@gmail.com>
2019-10-02 13:07:14 +0200

    courseinfo

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,28 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+const part1 = {
+  name: 'Fundamentals of React',
+  exercises: 10
+}
+const part2 = {
+  name: 'Using props to pass data',
+  exercises: 7
+}
+const part3 = {
+  name: 'State of a component',
+  exercises: 14
+}
+
+const Header = (props) => {
+  return (
+    <h1>
+      {props.course}
+    </h1>
+  );
+}
+
+const Part = (props) => {
+ 
+  return (
+    <div>
+      {props.name} {props.exercises}
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
 
   return (
     <div>
-      <h1>{course}</h1>
+      <Header course={course} />
       <p>
-        {part1} {exercises1}
+        <Part name={part1.name} exercises={part1.exercises} />
+        <Part name={part2.name} exercises={part2.exercises} />
+        <Part name={part3.name} exercises={part3.exercises} />
       </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     </div>
   )
 }
