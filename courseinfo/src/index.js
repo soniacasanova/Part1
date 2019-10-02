35abc873e25a85bd13662d589d732b3ef80c96b6
“soniacasanova” <soniacasanovar@gmail.com>
2019-10-02 18:14:12 +0200

    course

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -2,51 +2,86 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-const part1 = {
-  name: 'Fundamentals of React',
-  exercises: 10
-}
-const part2 = {
-  name: 'Using props to pass data',
-  exercises: 7
-}
-const part3 = {
-  name: 'State of a component',
-  exercises: 14
-}
-
 const Header = (props) => {
   return (
     <h1>
-      {props.course}
+      {props.name}
     </h1>
   );
 }
 
 const Part = (props) => {
- 
-  return (
+  const receivedPart = props.part; 
+ return (
     <div>
-      {props.name} {props.exercises}
+      {receivedPart.title} {receivedPart.exerciseCount}
     </div>
   )
 }
 
-const App = () => {
-  const course = 'Half Stack application development'
-
-
+const Content = (props) => {
+  const allParts = props.contentsParts;
+  console.log("is it shtg in allParts?" + allParts);
+  console.log("exercise? " + allParts[0].exerciseCount);
+  console.log("exercise? " + allParts[0].title);
+  
   return (
-    <div>
-      <Header course={course} />
-      <p>
-        <Part name={part1.name} exercises={part1.exercises} />
-        <Part name={part2.name} exercises={part2.exercises} />
-        <Part name={part3.name} exercises={part3.exercises} />
-      </p>
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
-    </div>
+    
+     <div>
+       <Part part={allParts[0]} />
+       <Part part={allParts[1]} />
+       <Part part={allParts[2]} />
+     </div>
+   )
+ }
+
+const Total = (props) => {
+  const allParts = props.parts;
+  const exerciseOfPart1 = allParts[0].exerciseCount;
+  const exerciseOfPart2 = allParts[0].exerciseCount;
+  const exerciseOfPart3 = allParts[0].exerciseCount;
+  
+  const totalExercises= exerciseOfPart1 + exerciseOfPart2 + exerciseOfPart3;
+  
+  return (
+    <p>
+  Total {totalExercises}
+    </p>
   )
+}
+
+// ---------------Main logic happens ----------------------------
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        title: 'Fundamentals of React',
+        exerciseCount: 10
+      },
+      {
+        title: 'Using props to pass data',
+        exerciseCount: 7
+      },
+      {
+        title: 'State of a component',
+        exerciseCount: 14
+      }
+    ]
+  }
+
+
+let totalexerciseCount = 0;
+
+return (
+  <div>
+    <div>
+      <Header name={course.name} />
+      <Content contentsParts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  </div>
+)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
